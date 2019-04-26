5943d1d62c4a22f3028529873e047a65df819263
Sergio Zambrano <sergio.zambrano@enalquiler.com>
2019-04-26 08:14:05 +0200

    1) Sustitución de valores enteros y literales por constantes

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,52 +4,64 @@ namespace GildedRose;
 
 class GildedRose {
 
-	public static function updateQuality(
+    const MINIMUM_DECREASE_QUALITY = 1;
+    const MINIMUM_INCREASE_QUALITY = 1;
+    const MINIMUM_QUALITY = 0;
+    const MAXIMUM_QUALITY = 50;
+    const MINIMUM_SELL_IN = 0;
+    const MINIMUM_DECREASE_SELL_IN = 1;
+    const FIRST_BACKSTAGE_LIMIT = 6;
+    const SECOND_BACKSTAGE_LIMIT = 11;
+    const AGED_BRIE = "Aged Brie";
+    const BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    const SULFURAS = "Sulfuras, Hand of Ragnaros";
+
+    public static function updateQuality(
 		$items
 	) {
 		for ($i = 0; $i < count($items); $i++) {
-			if (("Aged Brie" != $items[$i]->getName()) && ("Backstage passes to a TAFKAL80ETC concert" != $items[$i]->getName())) {
-				if ($items[$i]->getQuality() > 0) {
-					if ("Sulfuras, Hand of Ragnaros" != $items[$i]->getName()) {
-						$items[$i]->setQuality($items[$i]->getQuality() - 1);
+			if ((self::AGED_BRIE != $items[$i]->getName()) && (self::BACKSTAGE != $items[$i]->getName())) {
+				if ($items[$i]->getQuality() > self::MINIMUM_QUALITY) {
+					if (self::SULFURAS != $items[$i]->getName()) {
+						$items[$i]->setQuality($items[$i]->getQuality() - self::MINIMUM_DECREASE_QUALITY);
 					}
 				}
 			} else {
-				if ($items[$i]->getQuality() < 50) {
-					$items[$i]->setQuality($items[$i]->getQuality() + 1);
-					if ("Backstage passes to a TAFKAL80ETC concert" == $items[$i]->getName()) {
-						if ($items[$i]->getSellIn() < 11) {
-							if ($items[$i]->getQuality() < 50) {
-								$items[$i]->setQuality($items[$i]->getQuality() + 1);
+				if ($items[$i]->getQuality() < self::MAXIMUM_QUALITY) {
+					$items[$i]->setQuality($items[$i]->getQuality() + self::MINIMUM_INCREASE_QUALITY);
+					if (self::BACKSTAGE == $items[$i]->getName()) {
+						if ($items[$i]->getSellIn() < self::SECOND_BACKSTAGE_LIMIT) {
+							if ($items[$i]->getQuality() < self::MAXIMUM_QUALITY) {
+								$items[$i]->setQuality($items[$i]->getQuality() + self::MINIMUM_INCREASE_QUALITY);
 							}
 						}
-						if ($items[$i]->getSellIn() < 6) {
-							if ($items[$i]->getQuality() < 50) {
-								$items[$i]->setQuality($items[$i]->getQuality() + 1);
+						if ($items[$i]->getSellIn() < self::FIRST_BACKSTAGE_LIMIT) {
+							if ($items[$i]->getQuality() < self::MAXIMUM_QUALITY) {
+								$items[$i]->setQuality($items[$i]->getQuality() + self::MINIMUM_INCREASE_QUALITY);
 							}
 						}
 					}
 				}
 			}
 
-			if ("Sulfuras, Hand of Ragnaros" != $items[$i]->getName()) {
-				$items[$i]->setSellIn($items[$i]->getSellIn() - 1);
+			if (self::SULFURAS != $items[$i]->getName()) {
+				$items[$i]->setSellIn($items[$i]->getSellIn() - self::MINIMUM_DECREASE_SELL_IN);
 			}
 
-			if ($items[$i]->getSellIn() < 0) {
-				if ("Aged Brie" != $items[$i]->getName()) {
-					if ("Backstage passes to a TAFKAL80ETC concert" != $items[$i]->getName()) {
-						if ($items[$i]->getQuality() > 0) {
-							if ("Sulfuras, Hand of Ragnaros" != $items[$i]->getName()) {
-								$items[$i]->setQuality($items[$i]->getQuality() - 1);
+			if ($items[$i]->getSellIn() < self::MINIMUM_SELL_IN) {
+				if (self::AGED_BRIE != $items[$i]->getName()) {
+					if (self::BACKSTAGE != $items[$i]->getName()) {
+						if ($items[$i]->getQuality() > self::MINIMUM_QUALITY) {
+							if (self::SULFURAS != $items[$i]->getName()) {
+								$items[$i]->setQuality($items[$i]->getQuality() - self::MINIMUM_DECREASE_QUALITY);
 							}
 						}
 					} else {
 						$items[$i]->setQuality($items[$i]->getQuality() - $items[$i]->getQuality());
 					}
 				} else {
-					if ($items[$i]->getQuality() < 50) {
-						$items[$i]->setQuality($items[$i]->getQuality() + 1);
+					if ($items[$i]->getQuality() < self::MAXIMUM_QUALITY) {
+						$items[$i]->setQuality($items[$i]->getQuality() + self::MINIMUM_INCREASE_QUALITY);
 					}
 				}
 			}
